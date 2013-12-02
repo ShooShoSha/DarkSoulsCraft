8ba90c2a72aa2a37af67e0ce4f301c9a55b23961
shooshosha <shooshosha@gmail.com>
2013-12-02 08:32:30 -0600

    Trying different referencing technique.

--- common/com/shooshosha/darksouls/DarkSoulsCraft.java
+++ common/com/shooshosha/darksouls/DarkSoulsCraft.java
@@ -9,15 +9,9 @@
  */
 package com.shooshosha.darksouls;
 
-import java.util.logging.Level;
-
-import net.minecraftforge.common.Configuration;
-
 import com.shooshosha.darksouls.core.proxy.CommonProxy;
 import com.shooshosha.darksouls.lib.Reference;
-import com.shooshosha.darksouls.lib.Version;
 
-import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -33,12 +27,12 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
  */
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, dependencies = Reference.MOD_DEPENDENCIES)
 public class DarkSoulsCraft {
-	@Instance
+	@Instance(Reference.MOD_ID)
 	public static DarkSoulsCraft instance;
 	
 	@SidedProxy(clientSide=Reference.PROXY_CLIENT, serverSide = Reference.PROXY_SERVER)
 	public static CommonProxy proxy;
-	
+		
 	@EventHandler public void preInit(FMLPreInitializationEvent event) {
 		
 	}
