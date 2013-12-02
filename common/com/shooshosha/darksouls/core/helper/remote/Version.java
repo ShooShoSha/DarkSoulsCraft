1936ae2aeaaaa99f926bac4729e99bfe28af806c
shooshosha <shooshosha@gmail.com>
2013-12-02 09:56:39 -0600

    Version checkers for local and remote validation.

---
+++ common/com/shooshosha/darksouls/core/helper/remote/Version.java
@@ -0,0 +1,29 @@
+/**
+ * DarkSoulsCraft
+ * 
+ * Version.java
+ * 
+ * @author shooshosha
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+package com.shooshosha.darksouls.core.helper.remote;
+
+/**
+ * @author shooshosha
+ * @date Dec 2, 2013
+ *
+ */
+public class Version implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void execute() {
+		
+	}
+
+}
