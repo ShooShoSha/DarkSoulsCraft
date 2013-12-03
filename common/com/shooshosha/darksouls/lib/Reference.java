4bf622b495beb3016263287f496a1fc3fe909f06
shooshosha <shooshosha@gmail.com>
2013-12-03 08:15:15 -0600

    Added version control constants.

--- common/com/shooshosha/darksouls/lib/Reference.java
+++ common/com/shooshosha/darksouls/lib/Reference.java
@@ -15,12 +15,19 @@ package com.shooshosha.darksouls.lib;
  *
  */
 public class Reference {
+	//Mod specific constants
 	public static final String MOD_ID = "darksoulscraft";
 	public static final String MOD_NAME = "Dark Souls Craft";
-	public static final String MOD_DEPENDENCIES = "required_after:FML";
+	public static final String MOD_DEPENDENCIES = "required_after:Forge@[9.11.1.953,)";
+	public static final String MOD_VERSION = "@VERSION@:@BUILD@";
 	
+	//Version control constants
+	public static final int VERSION_CHECK_ATTEMPTS = 3;
+	
+	//Proxy class constants
 	public static final String PROXY_CLIENT	 = "com.shooshosha.darksouls.core.proxy.ClientProxy";
 	public static final String PROXY_SERVER  = "com.shooshosha.darksouls.core.proxy.ServerProxy";
 	
+	//Jar signature constants
 	public static final String FINGERPRINT = "@FINGERPRINT@";
 }
