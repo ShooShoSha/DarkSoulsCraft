46f251569dab9b06c7896eeaea53a90dc5f06f10
shooshosha <shooshosha@gmail.com>
2013-12-02 19:39:41 -0600

    Created class for various string messages.

---
+++ common/com/shooshosha/darksouls/lib/Messages.java
@@ -0,0 +1,7 @@
+package com.shooshosha.darksouls.lib;
+
+public class Messages {
+	//Fingerprint error messages
+	public static final String FINGERPRINT_NONE = "fingerprint.dsc:none";
+	public static final String FINGERPRINT_INVALID = "fingerprint.dsc:invalid";
+}
