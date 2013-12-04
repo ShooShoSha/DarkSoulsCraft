4326fc6ce44af8924ff10d6bc939922e39a77a28
kobrien <kobrien@P54568.college.local>
2013-12-04 13:37:05 -0600

    Renamed

---
+++ common/com/shooshosha/darksouls/config/ConfigurationSettings.java
@@ -0,0 +1,20 @@
+/**
+ * DarkSoulsCraft
+ * 
+ * ConfigurationSettings.java
+ * 
+ * @author shooshosha
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+package com.shooshosha.darksouls.config;
+
+/**
+ * @author shooshosha
+ * @date Dec 2, 2013
+ *
+ */
+public class ConfigurationSettings {
+	//Version check related settings
+	
+}
