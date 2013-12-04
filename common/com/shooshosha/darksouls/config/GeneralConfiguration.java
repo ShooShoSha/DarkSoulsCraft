1f2b303ba3cb7c8afa66799688e6f0d0928c1626
kobrien <kobrien@P54568.college.local>
2013-12-04 13:28:35 -0600

    Created file.

---
+++ common/com/shooshosha/darksouls/config/GeneralConfiguration.java
@@ -0,0 +1,19 @@
+/**
+ * DarkSoulsCraft
+ * 
+ * GeneralConfiguration.java
+ * 
+ * @author shooshosha
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+package com.shooshosha.darksouls.config;
+
+/**
+ * @author shooshosha
+ * @date Dec 4, 2013
+ *
+ */
+public class GeneralConfiguration {
+
+}
