db05e14b1e93de40744d16939b69e8a37849cb82
kobrien <kobrien@P54568.college.local>
2013-12-04 08:37:04 -0600

    Add version control messages.

--- common/com/shooshosha/darksouls/lib/Messages.java
+++ common/com/shooshosha/darksouls/lib/Messages.java
@@ -4,4 +4,14 @@ public class Messages {
 	//Fingerprint error messages
 	public static final String FINGERPRINT_NONE = "fingerprint.dsc:none";
 	public static final String FINGERPRINT_INVALID = "fingerprint.dsc:invalid";
+	
+	//Version check status messages
+	public static final String VERSION_INIT = "version.dsc:init";
+	public static final String VERSION_UNINIT = "version.dsc:uninit";
+	public static final String VERSION_CURRENT = "version.dsc:current";
+	public static final String VERSION_OUTDATED = "version.dsc:outdated";
+	public static final String VERSION_ERROR_CONNECT = "version.dsc:error_connect";
+	public static final String VERSION_ERROR_ATTEMPT = "version.dsc:error_attempt";
+	public static final String VERSION_ERROR_MC = "version.dsc:error_mc";
+	public static final String VERSION_ERROR_UNKNOWN = "version.dsc:error_unknown";
 }
