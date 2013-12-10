d58bc392261628caa7aadec3012cc540684744f5
kobrien <kobrien@P54568.college.local>
2013-12-10 11:15:37 -0600

    Added init with version configuration reads

--- common/com/shooshosha/darksouls/config/GeneralConfiguration.java
+++ common/com/shooshosha/darksouls/config/GeneralConfiguration.java
@@ -9,6 +9,16 @@
  */
 package com.shooshosha.darksouls.config;
 
+import static net.minecraftforge.common.Configuration.CATEGORY_GENERAL;
+
+import java.io.File;
+import java.util.logging.Level;
+
+import com.shooshosha.darksouls.lib.Messages;
+import com.shooshosha.darksouls.lib.Reference;
+
+import cpw.mods.fml.common.FMLLog;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.Configuration;
 
 /**
@@ -18,6 +28,24 @@ import net.minecraftforge.common.Configuration;
  */
 public class GeneralConfiguration {
 	private static Configuration generalConfiguration;
+	
+	protected static void init(File configFile) {
+		generalConfiguration = new Configuration(configFile);
+		
+		try {
+			generalConfiguration.load();
+			
+			/* Version check */
+			ConfigurationSettings.VERSION_RESULT = generalConfiguration.get(CATEGORY_GENERAL, ConfigurationSettings.VERSION_RESULT_NAME, ConfigurationSettings.VERSION_RESULT_DEFAULT).getBoolean(ConfigurationSettings.VERSION_RESULT_DEFAULT);
+			ConfigurationSettings.VERSION_DISCOVERED = generalConfiguration.get(CATEGORY_GENERAL, ConfigurationSettings.VERSION_DISCOVERED_NAME, ConfigurationSettings.VERSION_DISCOVERED_DEFAULT).getString();
+			ConfigurationSettings.VERSION_TYPE = generalConfiguration.get(CATEGORY_GENERAL, ConfigurationSettings.VERSION_TYPE_NAME, ConfigurationSettings.VERSION_TYPE_DEFAULT).getString();
+			
+		} catch (Exception e) {
+			FMLLog.log(Level.SEVERE, e, StatCollector.translateToLocalFormatted(Messages.CONFIG_READ, Reference.MOD_NAME));
+		} finally {
+			generalConfiguration.save();
+		}
+	}
 	
 	public static void set(String categoryName, String propertyName, String newValue) {
 		generalConfiguration.load();
