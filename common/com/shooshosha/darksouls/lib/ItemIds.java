1954d56373597c19199a7f5626ed10f12e0bfcb6
shooshosha <shooshosha@gmail.com>
2013-12-10 11:52:15 -0600

    Created item IDs class with first item.

---
+++ common/com/shooshosha/darksouls/lib/ItemIds.java
@@ -0,0 +1,23 @@
+/**
+ * DarkSoulsCraft
+ * 
+ * ItemIds.java
+ * 
+ * @author shooshosha
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+package com.shooshosha.darksouls.lib;
+
+/**
+ * @author shooshosha
+ * @date Dec 10, 2013
+ *
+ */
+public class ItemIds {
+	/* Default item IDs */
+	public static final int HOMEWARD_BONE_DEFAULT = 25400;
+	
+	/* Current item IDs */
+	public static int HOMEWARD_BONE;
+}
