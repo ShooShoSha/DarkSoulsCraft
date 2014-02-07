b337f3f843933571a249f29157857aed54dce4b3
kobrien <kobrien@P54568.college.local>
2014-02-07 15:40:22 -0600

    Added attempts localization messages.

--- common/com/shooshosha/darksouls/lib/Messages.java
+++ common/com/shooshosha/darksouls/lib/Messages.java
@@ -10,8 +10,10 @@ public class Messages {
 	
 	//Version check status messages
 	public static final String VERSION_INITIALIZATION = "version.dsc:initialization";
+	public static final String VERSION_UNINITIALIZED = "version.dsc:uninitialized";
 	public static final String VERSION_URL_BAD = "version.dsc:url_bad";
 	public static final String VERSION_FAILED_CONNECTION = "version.dsc:failed_connection";
+	public static final String VERSION_ATTEMPTS = "version.dsc:attempts";
 	public static final String VERSION_REMOTE_FILE_INVALID = "version.dsc:remote_file_invalid";
 	public static final String VERSION_KEY_INVALID = "version.dsc:key_invalid";
 	public static final String VERSION_OUTDATED = "version.dsc:outdated";
