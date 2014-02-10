81cbc910636e1aabf54b51b2de05aa8a6dee09c7
kobrien <kobrien@P54568.college.local>
2014-02-10 15:00:31 -0600

    Refactored constants using enums.

---
+++ common/com/shooshosha/darksouls/item/ItemIds.java
@@ -0,0 +1,19 @@
+/**
+ * DarkSoulsCraft
+ * 
+ * ItemIds.java
+ * 
+ * @author shooshosha
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+package com.shooshosha.darksouls.item;
+
+/**
+ * @author shooshosha
+ * @date Dec 10, 2013
+ *
+ */
+public class ItemIds {	
+	public static int HOMEWARDBONE;
+}
