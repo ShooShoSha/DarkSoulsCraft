a5cf943ad2c6fff321464ecc790611289e184468
ShooShoSha <shooshosha@gmail.com>
2014-02-20 07:52:53 -0600

    Moved file. Organized file into weapons/halberd package.

---
+++ src/main/java/com/shooshosha/darksouls/item/weapon/halberd/Halberd.java
@@ -0,0 +1,10 @@
+package com.shooshosha.darksouls.item.weapon.halberd;
+
+
+public class Halberd extends Halberds {
+	public Halberd() {
+		super();
+		setUnlocalizedName("halberd");
+		setTextureName(getUnlocalizedName());
+	}
+}
