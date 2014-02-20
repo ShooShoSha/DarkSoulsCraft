7899cae56ff517d34a8a2b075b8e0ef05c9c70a9
ShooShoSha <shooshosha@gmail.com>
2014-02-20 07:52:18 -0600

    Added file. Abstracted halberds.

---
+++ src/main/java/com/shooshosha/darksouls/item/weapon/halberd/Halberds.java
@@ -0,0 +1,9 @@
+package com.shooshosha.darksouls.item.weapon.halberd;
+
+import com.shooshosha.darksouls.item.weapon.Weapon;
+
+public abstract class Halberds extends Weapon {
+	public Halberds() {
+		super();
+	}
+}
