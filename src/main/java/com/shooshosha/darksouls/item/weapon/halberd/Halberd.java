086144bb43f40e78cdba06ada88981918de6960a
ShooShoSha <shooshosha@gmail.com>
2014-02-20 15:51:24 -0600

    Added statuses.

--- src/main/java/com/shooshosha/darksouls/item/weapon/halberd/Halberd.java
+++ src/main/java/com/shooshosha/darksouls/item/weapon/halberd/Halberd.java
@@ -1,10 +1,26 @@
 package com.shooshosha.darksouls.item.weapon.halberd;
 
+import com.shooshosha.darksouls.item.weapon.Bonus;
+
+
 
 public class Halberd extends Halberds {
 	public Halberd() {
 		super();
 		setUnlocalizedName("halberd");
 		setTextureName(getUnlocalizedName());
+		setBaseStatuses();
+	}
+
+	protected void setBaseStatuses() {
+		setDamages(110,0,0,0);
+		setReductions(40,10,30,30);
+		setRequirements(16,12,0,0);
+		setBonuses(Bonus.D, Bonus.D, Bonus.Z, Bonus.Z);
+		setTypes(true, false, false, true);
+		setCritcalDamage(100);
+		setWeight(60);
+		setDurability(200);
+		setSoulValue(50);
 	}
 }
--- src/main/java/com/shooshosha/darksouls/item/weapon/halberd/Halberds.java
+++ src/main/java/com/shooshosha/darksouls/item/weapon/halberd/Halberds.java
@@ -2,8 +2,9 @@ package com.shooshosha.darksouls.item.weapon.halberd;
 
 import com.shooshosha.darksouls.item.weapon.Weapon;
 
-public abstract class Halberds extends Weapon {
-	public Halberds() {
+
+abstract class Halberds extends Weapon {
+	protected Halberds() {
 		super();
 	}
 }
