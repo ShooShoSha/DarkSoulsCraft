74804ea291c0a04c9e77e46ab31606d7175361fb
ShooShoSha <shooshosha@gmail.com>
2014-02-20 07:54:49 -0600

    Override method. Implemented different setUnlocalizedName to concatenate resource prefix in References to the name from the class.

--- src/main/java/com/shooshosha/darksouls/item/weapon/Weapon.java
+++ src/main/java/com/shooshosha/darksouls/item/weapon/Weapon.java
@@ -1,14 +1,27 @@
 package com.shooshosha.darksouls.item.weapon;
 
+import net.minecraft.item.Item;
+
 import com.shooshosha.darksouls.item.DSCItem;
+import com.shooshosha.darksouls.lib.Reference;
 
 public abstract class Weapon extends DSCItem {
 	private int physicalDamage;
-
+	
+	public Weapon() {
+		super();
+		setMaxStackSize(1);
+	}
+	
+	@Override
+	public Item setUnlocalizedName(String unlocalName) {
+		return super.setUnlocalizedName(Reference.RESOURCE_PREFIX + unlocalName);
+	}
+	
 	public int getPhysicalDamage() {
 		return physicalDamage;
 	}
-
+	
 	public void setPhysicalDamage(int physicalDamage) {
 		this.physicalDamage = physicalDamage;
 	}
