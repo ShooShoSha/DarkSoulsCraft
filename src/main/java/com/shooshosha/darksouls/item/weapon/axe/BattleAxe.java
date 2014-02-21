f499790f5d2c389e1764be7bd1605621cfd216e5
ShooShoSha <shooshosha@gmail.com>
2014-02-21 14:32:17 -0600

    Edit BattleAxe
    Include stats for weapon.

--- src/main/java/com/shooshosha/darksouls/item/weapon/axe/BattleAxe.java
+++ src/main/java/com/shooshosha/darksouls/item/weapon/axe/BattleAxe.java
@@ -1,5 +1,7 @@
 package com.shooshosha.darksouls.item.weapon.axe;
 
+import com.shooshosha.darksouls.item.weapon.Bonus;
+
 public class BattleAxe extends Axes {
 	public BattleAxe() {
 		super();
@@ -9,8 +11,16 @@ public class BattleAxe extends Axes {
 	}
 	@Override
 	protected void setBaseStatuses() {
-		// TODO Auto-generated method stub
-
+		setDamages(95, 0, 0, 0);
+		setReductions(55, 10, 40, 40);
+		setRequirements(10, 0, 0, 0);
+		setBonuses(Bonus.C, Bonus.D, Bonus.Z, Bonus.Z);
+		setTypes(false, true, false, false);
+		setCriticalDamage(100);
+		setWeight(40);
+		setDurability(250);
+		setSoulValue(0);
+		setStability(36);
 	}
 
 }
