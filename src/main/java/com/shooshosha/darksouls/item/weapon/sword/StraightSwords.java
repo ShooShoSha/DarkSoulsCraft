310aa37673b7df9d044744ed89a15cca16a39485
ShooShoSha <shooshosha@gmail.com>
2014-02-21 11:45:41 -0600

    Move methods to super class.
    Refactor methods setPath and setModifier to Weapon super class. These
    are universal statuses across all weapons. Exceptions apply to specific
    weapons and should be reflected at construction.

--- src/main/java/com/shooshosha/darksouls/item/weapon/sword/StraightSwords.java
+++ src/main/java/com/shooshosha/darksouls/item/weapon/sword/StraightSwords.java
@@ -1,12 +1,9 @@
 package com.shooshosha.darksouls.item.weapon.sword;
 
-import com.shooshosha.darksouls.item.weapon.Upgrade;
 import com.shooshosha.darksouls.item.weapon.Weapon;
 
 abstract class StraightSwords extends Weapon {
 	public StraightSwords() {
 		super();
-		setPath(Upgrade.NORMAL);
-		setModifier(0);
 	}
 }
