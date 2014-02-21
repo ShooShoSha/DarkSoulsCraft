3206adef9284893023f9010b9b47b8667ed6c4eb
ShooShoSha <shooshosha@gmail.com>
2014-02-21 14:10:48 -0600

    Add weapon.
    Include Battle Axe in game armory.

---
+++ src/main/java/com/shooshosha/darksouls/item/weapon/axe/BattleAxe.java
@@ -0,0 +1,16 @@
+package com.shooshosha.darksouls.item.weapon.axe;
+
+public class BattleAxe extends Axes {
+	public BattleAxe() {
+		super();
+		setUnlocalizedName("battleaxe");
+		setTextureName(getUnlocalizedName());
+		setBaseStatuses();
+	}
+	@Override
+	protected void setBaseStatuses() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
