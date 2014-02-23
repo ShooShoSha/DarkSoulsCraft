e000af5eb8754f6f20421346cea989dfda50c7b0
shooshosha <shooshosha@gmail.com>
2014-02-23 17:33:31 -0600

    Add range field.
    Specify bows have an effective range.

--- src/main/java/com/shooshosha/darksouls/item/weapon/bow/Bows.java
+++ src/main/java/com/shooshosha/darksouls/item/weapon/bow/Bows.java
@@ -1,9 +1,21 @@
 package com.shooshosha.darksouls.item.weapon.bow;
 
+
 import com.shooshosha.darksouls.item.weapon.Weapon;
+import com.shooshosha.darksouls.lib.Reference;
 
 abstract class Bows extends Weapon {
+	private int range;
 	Bows() {
 		super();
+	}
+	
+	public final void setRange(int range) {
+		if(!Reference.isNegative(range))
+			this.range = range;
+	}
+	
+	public final int getRange() {
+		return range;
 	}
 }
