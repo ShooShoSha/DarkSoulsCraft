4ec45abee7dc7fc50377f67979dbb324dce25e48
ShooShoSha <shooshosha@gmail.com>
2014-02-24 14:16:51 -0600

    Add abstraction.
    Added abstraction template for crossbows.

---
+++ src/main/java/com/shooshosha/darksouls/item/weapon/crossbow/Crossbows.java
@@ -0,0 +1,21 @@
+package com.shooshosha.darksouls.item.weapon.crossbow;
+
+import com.shooshosha.darksouls.item.weapon.Weapon;
+import com.shooshosha.darksouls.lib.Reference;
+
+abstract class Crossbows extends Weapon {
+	private int range;
+	
+	Crossbows() {
+		super();
+	}
+	
+	public final void setRange(int range) {
+		if(!Reference.isNegative(range))
+			this.range = range;
+	}
+	
+	public final int getRange() {
+		return range;
+	}
+}
