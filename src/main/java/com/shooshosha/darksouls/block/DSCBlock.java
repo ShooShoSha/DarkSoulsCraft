e3db19dd51410d7a1596f11ffd105c88aad99429
--replace-all <shooshosha@gmail.com>
2014-06-19 08:51:36 -0500

    Include license notice.

--- src/main/java/com/shooshosha/darksouls/block/DSCBlock.java
+++ src/main/java/com/shooshosha/darksouls/block/DSCBlock.java
@@ -1,11 +1,19 @@
 /**
- * DarkSoulsCraft
- * 
- * BlockDS.java
- * 
- * @author shooshosha
- * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * Copyright 2013 Kevin O'Brien (shooshosha@gmail.com) (License: LGPLv3 or later)
  *
+ * This file is part of Dark Souls Craft.
+ *
+ * Dark Souls Craft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Dark Souls Craft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dark Souls Craft.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.shooshosha.darksouls.block;
 
