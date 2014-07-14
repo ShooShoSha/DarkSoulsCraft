855f547abd36abbf25b11a50285a35f33befa0ed
--replace-all <shooshosha@gmail.com>
2014-07-14 17:13:43 -0500

    Update constructor.

--- src/main/java/com/shooshosha/darksouls/item/HomewardBone.java
+++ src/main/java/com/shooshosha/darksouls/item/HomewardBone.java
@@ -1,8 +1,26 @@
+/*
+ * Copyright 2014. Kevin O'Brien (shooshosha@gmail.com) License: LGPLv3 or later
+ *
+ * This file is part of DarkSoulsCraft.
+ *
+ * DarkSoulsCraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * DarkSoulsCraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DarkSoulsCraft.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * DarkSoulsCraft
- * 
+ *
  * HomewardBone.java
- * 
+ *
  * @author shooshosha
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  *
@@ -10,18 +28,15 @@
 package com.shooshosha.darksouls.item;
 
 
-
 /**
  * @author shooshosha
  * @date Dec 11, 2013
- *
  */
 public class HomewardBone extends DSCItem {
-	
-	public HomewardBone() {
-		super();
-		setUnlocalizedName("dsc:homewardbone");
-		setTextureName(getUnlocalizedName());
-		maxStackSize = 99; //This probably won't work
-	}
+
+    public HomewardBone() {
+        super("homewardbone");
+        setTextureName(getUnlocalizedName());
+        maxStackSize = 99; //This probably won't work
+    }
 }
