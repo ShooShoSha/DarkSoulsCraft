5876048e5b164909cabe07e4c13f7c7e415359d5
--replace-all <shooshosha@gmail.com>
2014-07-14 17:11:51 -0500

    Create constants for package location of sided proxies.

--- src/main/java/com/shooshosha/darksouls/proxy/Proxy.java
+++ src/main/java/com/shooshosha/darksouls/proxy/Proxy.java
@@ -1,14 +1,33 @@
+/*
+ * Copyright 2014. Kevin O'Brien (shooshosha@gmail.com) License: LGPLv3 or later
+ *
+ * This file is part of DarkSoulsCraft.
+ *
+ * DarkSoulsCraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * DarkSoulsCraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DarkSoulsCraft.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * DarkSoulsCraft
- * 
+ *
  * CommonProxy.java
- * 
+ *
  * @author shooshosha
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  *
  */
 package com.shooshosha.darksouls.proxy;
 
+import com.shooshosha.darksouls.DarkSoulsCraft;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLFingerprintViolationEvent;
 import cpw.mods.fml.common.event.FMLInterModComms;
@@ -18,11 +37,12 @@ import com.shooshosha.darksouls.creativetab.DSCCreativeTab;
 
 /**
  * @author shooshosha
- * @date Dec 2, 2013
- *
  */
 public abstract class Proxy {
-	public static CreativeTabs DSCtab = new DSCCreativeTab(Reference.MOD_ID);
+    public static final String CLIENT = "com.shooshosha.darksouls.proxy.Client";
+    public static final String SERVER = "com.shooshosha.darksouls.proxy.Server";
+
+    public static CreativeTabs DSCtab = new DSCCreativeTab(DarkSoulsCraft.ID);
 
     @EventHandler
     public void onTampered(FMLFingerprintViolationEvent event) {
