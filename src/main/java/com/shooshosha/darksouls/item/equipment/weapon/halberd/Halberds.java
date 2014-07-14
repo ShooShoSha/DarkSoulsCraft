a5f82e60755397e3a678e303579dbda331e525f5
--replace-all <shooshosha@gmail.com>
2014-07-14 17:12:31 -0500

    Update Halberds statistics and construction.

--- src/main/java/com/shooshosha/darksouls/item/equipment/weapon/halberd/Halberds.java
+++ src/main/java/com/shooshosha/darksouls/item/equipment/weapon/halberd/Halberds.java
@@ -1,10 +1,29 @@
+/*
+ * Copyright 2014. Kevin O'Brien (shooshosha@gmail.com) License: LGPLv3 or later
+ *
+ * This file is part of DarkSoulsCraft.
+ *
+ * DarkSoulsCraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * DarkSoulsCraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DarkSoulsCraft.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.shooshosha.darksouls.item.equipment.weapon.halberd;
 
 import com.shooshosha.darksouls.item.equipment.weapon.DSCItemWeapon;
 
 
 abstract class Halberds extends DSCItemWeapon {
-	protected Halberds() {
-		super();
-	}
+    protected Halberds(String unlocalName) {
+        super("halberds." + unlocalName);
+    }
+    //if attack doesn't connect, player stumbles
 }
