c2bc328f80f7ba19bad324148dd8535fff395962
--replace-all <shooshosha@gmail.com>
2014-07-14 08:13:31 -0500

    Rename method.

--- src/main/java/com/shooshosha/darksouls/library/Registrable.java
+++ src/main/java/com/shooshosha/darksouls/library/Registrable.java
@@ -3,7 +3,7 @@ package com.shooshosha.darksouls.library;
 import net.minecraft.client.renderer.texture.IIconRegister;
 
 public interface Registrable {
-	void setName(String unlocal);
+	void setUnlocalNameAs(String unlocalName);
 	void setTextureResource();
 	void setIcon(IIconRegister iconRegsiter);
 }
