f235e2d5c4d25e8c896f68756e1d8c5f5ba58889
--replace-all <shooshosha@gmail.com>
2014-07-14 17:07:07 -0500

    Identify sided proxies.

--- src/main/java/com/shooshosha/darksouls/DarkSoulsCraft.java
+++ src/main/java/com/shooshosha/darksouls/DarkSoulsCraft.java
@@ -1,19 +1,19 @@
-/**
- * Copyright 2013 Kevin O'Brien (shooshosha@gmail.com) (License: LGPLv3 or later)
+/*
+ * Copyright 2014. Kevin O'Brien (shooshosha@gmail.com) License: LGPLv3 or later
  *
- * This file is part of Dark Souls Craft.
+ * This file is part of DarkSoulsCraft.
  *
- * Dark Souls Craft is free software: you can redistribute it and/or modify
+ * DarkSoulsCraft is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License.
  *
- * Dark Souls Craft is distributed in the hope that it will be useful,
+ * DarkSoulsCraft is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Dark Souls Craft.  If not, see <http://www.gnu.org/licenses/>.
+ * along with DarkSoulsCraft.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.shooshosha.darksouls;
 
@@ -45,6 +45,6 @@ public class DarkSoulsCraft {
 	@Instance(ID)
 	public static DarkSoulsCraft instance;
 
-	@SidedProxy
+    @SidedProxy(modId = ID, clientSide = Proxy.CLIENT, serverSide = Proxy.SERVER)
     public static Proxy proxy;
 }
