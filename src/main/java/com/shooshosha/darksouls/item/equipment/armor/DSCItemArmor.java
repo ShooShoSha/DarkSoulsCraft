cb5780a88fbe35baed27924e92dbfc5e4981ca4c
--replace-all <shooshosha@gmail.com>
2014-07-22 16:58:49 -0500

    Add set, constants, and constructor.

--- src/main/java/com/shooshosha/darksouls/item/equipment/armor/DSCItemArmor.java
+++ src/main/java/com/shooshosha/darksouls/item/equipment/armor/DSCItemArmor.java
@@ -1,13 +1,24 @@
 package com.shooshosha.darksouls.item.equipment.armor;
 
-import com.shooshosha.darksouls.DarkSoulsCraft;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemArmor;
 
-public class DSCItemArmor extends ItemArmor {
-    public static final String localePrefix = "armor." + DarkSoulsCraft.LOCALE;
+import java.util.HashSet;
+import java.util.Set;
 
-	public DSCItemArmor(ArmorMaterial material, int renderIndex_, int armorSlot) {
-		super(material, renderIndex_, armorSlot);
+public class DSCItemArmor extends ItemArmor {
+    public static final int HELM = 0;
+    public static final int CHEST = 1;
+    public static final int LEG = 2;
+    public static final int HAND = 3;
+
+    private static Set<DSCItemArmor> armors = new HashSet<DSCItemArmor>();
+
+	public DSCItemArmor(String unlocalName, ArmorMaterial material, int renderIndex, int armorSlot) {
+		super(material, renderIndex, armorSlot);
+        setUnlocalizedName("armor." + unlocalName);
+        setCreativeTab(CreativeTabs.tabCombat);
+        armors.add(this);
 	}
 
 }
