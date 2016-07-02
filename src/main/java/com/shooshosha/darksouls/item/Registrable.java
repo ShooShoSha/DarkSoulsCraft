baa00327067c1fc198b4b6535fff45b1aa761ddc
ShooShoSha <shooshosha@gmail.com>
2016-07-02 14:11:05 -0700

    Prepare for restructuring...

--- src/main/java/com/shooshosha/darksouls/item/Registrable.java
+++
@@ -1,7 +0,0 @@
-package com.shooshosha.darksouls.item;
-
-public interface Registrable {
-	void setName(String unlocalName);
-	void setTexture();
-	abstract void setIcon();
-}
--- src/main/java/com/shooshosha/darksouls/item/equipment/Upgrade.java
+++
@@ -1,5 +0,0 @@
-package com.shooshosha.darksouls.item.equipment;
-
-public enum Upgrade {
-	NORMAL,	DIVINE,	OCCULT, MAGIC, ENCHANTED, CRYSTAL, RAW, FIRE, CHAOS, LIGHTNING, BOSS, DEMON, DRAGON, UNIQUE;
-}
