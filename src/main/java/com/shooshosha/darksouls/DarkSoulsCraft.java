2e09d2e3fd9f73ce2cbc6f1d797be3fb6f65d629
ShooShoSha <shooshosha@gmail.com>
2016-07-03 19:06:37 -0700

    Add documentation.

--- src/main/java/com/shooshosha/darksouls/DarkSoulsCraft.java
+++ src/main/java/com/shooshosha/darksouls/DarkSoulsCraft.java
@@ -30,19 +30,47 @@ import net.minecraftforge.fml.common.event.*;
  */
 @Mod(modid = DarkSoulsCraft.ID, name = DarkSoulsCraft.NAME, dependencies = DarkSoulsCraft.DEPENDENCIES, guiFactory = ConfigGui.FACTORY)
 public class DarkSoulsCraft {
+    /**
+     * The name of the mod.
+     */
     public static final String NAME = "Dark Souls Craft";
+    /**
+     * Unique identifier for the mod.
+     */
     public static final String ID = "darksouls";
-    public static final String VERSION = "${mod_version}";
+    /**
+     * Mod's current version.
+     */
+    public static final String VERSION = "${version}";
+    /**
+     * Signed signature of mod.
+     */
     public static final String FINGERPRINT = "@FINGERPRINT@";
+    /**
+     * List of dependencies.
+     */
     public static final String DEPENDENCIES = "";
+    /**
+     * Identifies namespace for assets.
+     */
     public static final String LOCALE = ID + ".";
 
+    /**
+     * The singleton instance of this mod.
+     */
     @Mod.Instance(ID)
     public static DarkSoulsCraft instance;
 
+    /**
+     * The proxy instance.
+     */
     @SidedProxy(modId= ID, clientSide= Proxy.CLIENT, serverSide= Proxy.SERVER)
     public static Proxy proxy;
 
+    /**
+     * Event handler for mismatched fingerprint.
+     * @param event
+     */
     @Mod.EventHandler
     public void onTampered(FMLFingerprintViolationEvent event) {
         LogHelper.trace("Starting fingerprint violation handling");
@@ -50,6 +78,10 @@ public class DarkSoulsCraft {
         LogHelper.trace("Finished fingerprint violation handling");
     }
 
+    /**
+     * Event handler for instantiating objects.
+     * @param event
+     */
     @Mod.EventHandler
     public void preInitialize(FMLPreInitializationEvent event) {
         LogHelper.trace("Starting pre-initialization...");
@@ -57,6 +89,10 @@ public class DarkSoulsCraft {
         LogHelper.trace("Finished pre-initialization...");
     }
 
+    /**
+     * Event handler for registering objects and making requests.
+     * @param event
+     */
     @Mod.EventHandler
     public void onInitialize(FMLInitializationEvent event) {
         LogHelper.trace("Starting initialization...");
@@ -64,6 +100,10 @@ public class DarkSoulsCraft {
         LogHelper.trace("Finished initialization");
     }
 
+    /**
+     * Event handler to send messages to other mods.
+     * @param event
+     */
     @Mod.EventHandler
     public void sendMessages(FMLInterModComms event) {
         LogHelper.trace("Sending messages to other mods");
@@ -71,6 +111,10 @@ public class DarkSoulsCraft {
         LogHelper.trace("Sent messages to other mods");
     }
 
+    /**
+     * Event handler to receive messages sent by other mods.
+     * @param event
+     */
     @Mod.EventHandler
     public void receiveMessages(FMLInterModComms.IMCEvent event) {
         LogHelper.trace("Receiving message from other mod");
@@ -78,6 +122,10 @@ public class DarkSoulsCraft {
         LogHelper.trace("Received message from other mod");
     }
 
+    /**
+     * Event handler to perform cross-mod behaviours.
+     * @param event
+     */
     @Mod.EventHandler
     public void postInitialize(FMLPostInitializationEvent event) {
         LogHelper.trace("Starting post-initialization...");
@@ -92,6 +140,10 @@ public class DarkSoulsCraft {
         LogHelper.trace("Server about to start initialized");
     }
 
+    /**
+     * Event handler before a server starts.
+     * @param event
+     */
     @Mod.EventHandler
     public void onStart(FMLServerStartingEvent event) {
         LogHelper.trace("Server starting initialization");
@@ -99,6 +151,10 @@ public class DarkSoulsCraft {
         LogHelper.trace("Server starting initialized");
     }
 
+    /**
+     * Event handler for when a server starts.
+     * @param event
+     */
     @Mod.EventHandler
     public void postStart(FMLServerStartedEvent event) {
         LogHelper.trace("Server started initialization");
@@ -106,6 +162,10 @@ public class DarkSoulsCraft {
         LogHelper.trace("Server started initialized");
     }
 
+    /**
+     * Event handler for when a server is about to shutdown.
+     * @param event
+     */
     @Mod.EventHandler
     public void onStop(FMLServerStoppingEvent event) {
         LogHelper.trace("Server stopping finalizing");
@@ -113,6 +173,10 @@ public class DarkSoulsCraft {
         LogHelper.trace("Server stopping finalized");
     }
 
+    /**
+     * Event handler for when a server has stopped.
+     * @param event
+     */
     @Mod.EventHandler
     public void postStop(FMLServerStoppedEvent event) {
         LogHelper.trace("Server stopped finalizing");
